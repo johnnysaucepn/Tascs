74655c839cb52b23f1036a81c8efac62fed5a262
James Howat <james@thehowats.net>
2014-07-22 02:03:12 +0100

    Initial source import.

---
+++ Howatworks.Tascs.Core/PathUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Howatworks.Tascs.Core
+{
+    public static class PathUtils
+    {
+        public static string Root { get; set; }
+
+        public static string Resolve(string path)
+        {
+            var parts = path.Split('~');
+
+            return Path.Combine(Root, parts.Last().TrimStart(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar));
+        }
+
+    }
+}
---
+++ Howatworks.Tascs.Core/TascOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howatworks.Tascs.Core
+{
+    public class TascOptions : Dictionary<string,string>
+    {
+        public new string this[string key]
+        {
+            get {
+                return ContainsKey(key) ? base[key] : null;
+            }
+        }
+        
+    }
+}
