f369c2e107ccc80c8be1a5554da8c18ccc972664
james <james@thehowats.net>
2014-07-24 15:17:22 +0100

    Add some custom attributes to TascOptions, and some methods for merging them from default sets.

---
+++ Source/Howatworks.Tascs.Core/TascAlias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howatworks.Tascs.Core
+{
+    /*public sealed class TascAlias
+    {
+        private Func<Target, TascOptions> _tascAlias = null;
+
+        public TascAlias(Func<Target, TascOptions> func)
+        {
+            _tascAlias = func;
+        }
+
+        public Target Run();
+    {
+        return _tasc
+    }
+
+    }*/
+}
--- Source/Howatworks.Tascs.Trial/Program.cs
+++ Source/Howatworks.Tascs.Trial/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using Howatworks.Tascs.Core;
 using Howatworks.Tascs.MSBuild;
+using log4net.Config;
 
 namespace Howatworks.Tascs.Trial
 {
@@ -12,12 +13,34 @@ namespace Howatworks.Tascs.Trial
             // Get the user's current directory, not the path of the script
             PathUtils.Root = PathUtils.Resolve(Directory.GetCurrentDirectory(), @"..\..\..\..");
 
-            Target.Named("Default")
-                .BuildProject(@"Source\Howatworks.Tascs.Core\Howatworks.Tascs.Core.csproj", @"BuildOutput")
+            var defaultBuildOptions = new TascOptions<MSBuildOption>
+            {
+                {MSBuildOption.OutputPath, @"BuildOutput\Debug"},
+                {MSBuildOption.Configuration, @"Debug"},
+                {MSBuildOption.Platform, @"AnyCPU"},
+                {MSBuildOption.Targets, "Clean, Build"}
+            };
+
+
+            //Target.Named("Default")
+            //    .BuildProject(@"Source\Howatworks.Tascs.Core\Howatworks.Tascs.Core.csproj", @"BuildOutput")
                 //.ArchiveOutput(@"Source\Howatworks.Tascs.Core\bin\Debug\**");
                 //.GenerateAssemblyInfo("1.2.3.4")
-                .Exec(@"cmd.exe", Arg.Literal(@"/c"), Arg.Literal(@"echo"), Arg.Quoted(@"stupid wibble"));
-                
+            //    .Exec(@"cmd.exe", Arg.Literal(@"/c"), Arg.Literal(@"echo"), Arg.Quoted(@"stupid wibble"));
+
+            Target.Named("Weird")
+                .BuildProject(new TascOptions<MSBuildOption>
+                {
+                    {MSBuildOption.ProjectFilePath, @"Source\Howatworks.Tascs.Core\Howatworks.Tascs.Core.csproj"},
+                    {MSBuildOption.OutputPath, @"BuildOutput\Release"}
+                });
+
+            Target.Named("Minimal")
+                .BuildProject(TascOptions<MSBuildOption>.Merge(defaultBuildOptions, new TascOptions<MSBuildOption>
+                {
+                    {MSBuildOption.ProjectFilePath, @"Source\Howatworks.Tascs.MSBuild\Howatworks.Tascs.MSBuild.csproj"}
+                }));
+            
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
