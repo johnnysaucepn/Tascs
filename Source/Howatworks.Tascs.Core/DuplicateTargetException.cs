9d8ca26cc8c9301bbb15792f418aaa1a23907d71
james <james@thehowats.net>
2014-07-25 15:23:38 +0100

    Refactor, add dependency topological sorting.

---
+++ Source/Howatworks.Tascs.Core/DuplicateTargetException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howatworks.Tascs.Core
+{
+    public class DuplicateTargetException : Exception
+    {
+        private readonly string _name;
+
+        public DuplicateTargetException(string name)
+        {
+            _name = name;
+        }
+
+        public override string Message
+        {
+            get { return string.Format("Target '{0}' already exists", _name); }
+        }
+    }
+}
