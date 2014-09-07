489786609db5300a8263432e50eb965e82592d67
james <james@thehowats.net>
2014-09-08 00:03:11 +0100

    Extract subgraph of all targets that are dependencies on the one to be built, and build in correct order.

--- Source/Tests/ProjectTests/DependencyTests.cs
+++ Source/Tests/ProjectTests/DependencyTests.cs
@@ -7,30 +7,45 @@ namespace ProjectTests
     [TestClass]
     public class DependencyTests
     {
-        [TestMethod]
-        public void Topology()
+        [TestInitialize]
+        public void TreeGraph()
         {
+            TascProject.Instance.Target("A").Echo("A");
+            TascProject.Instance.Target("AA").DependsOn("A").Echo("AA");
+            TascProject.Instance.Target("AAA").DependsOn("AA").Echo("AAA");
+            TascProject.Instance.Target("AB").DependsOn("A").Echo("AB");
+            TascProject.Instance.Target("AC").DependsOn("A").Echo("AC");
+            TascProject.Instance.Target("B").Echo("B");
+            TascProject.Instance.Target("BA").DependsOn("B").Echo("BA");
+        }
 
-            TascProject.Instance
-                .Target("Build")
-                .Echo("Build");
-
-            TascProject.Instance
-                .Target("Deploy")
-                .DependsOn("Build")
-                .Echo("Deploy");
-
-            TascProject.Instance
-                .Target("Run")
-                .DependsOn("Deploy")
-                .Echo("Run");
-
-            TascProject.Instance
-                .Target("Report")
-                .DependsOn("Run")
-                .Echo("Report");
-
+        
+        [TestMethod]
+        public void Linear()
+        {
+            TascProject.Instance.Target("Build").Echo("Build");
+            TascProject.Instance.Target("Deploy").DependsOn("Build").Echo("Deploy");
+            TascProject.Instance.Target("Run").DependsOn("Deploy").Echo("Run");
+            TascProject.Instance.Target("Report").DependsOn("Run").Echo("Report");
             TascProject.Instance.Build("Report");
+        }
+
+        [TestMethod]
+        public void Tree1()
+        {
+            TascProject.Instance.Build("AAA");
+        }
+
+        [TestMethod]
+        public void Tree2()
+        {
+            TascProject.Instance.Build("BA");
+        }
+
+        [TestMethod]
+        public void Tree3()
+        {
+            TascProject.Instance.Build("AC");
         }
     }
 }
