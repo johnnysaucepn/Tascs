1d5cdb8c622a4e6937f51622ce10e6650725b2fd
James Howat <james@thehowats.net>
2014-10-23 13:57:18 +0100

    Fix BuildResult return type casting. ProjectFile should be parameter of MSBuildTasc, not MSBuildOptions.

---
+++ Source/Howatworks.Tascs.MSBuild/MSBuildResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Howatworks.Tascs.Core;
+using Microsoft.Build.Execution;
+
+namespace Howatworks.Tascs.MSBuild
+{
+    public class MSBuildResult : ITascResult
+    {
+        public BuildResult BuildResult { get; set; }
+
+        public MSBuildResult(BuildResult buildResult)
+        {
+            BuildResult = buildResult;
+        }
+    }
+}
